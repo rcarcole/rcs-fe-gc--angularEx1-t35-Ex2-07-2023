b1e4e2338ab1e8b715ac9e6fa39f81598659705f
rcarcole <rcarcole@hotmail.com>
2023-07-24 18:34:39 +0200

    subido

--- problema2/src/app/app.component.ts
+++ problema2/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'problema2';
+  numero1: number = 0;
+  numero2: number = 0;
+  suma: number = 0;
+  
+  ngOnInit() {
+    this.generarNumerosAleatorios();
+  }
+  
+  generarNumerosAleatorios() {
+    this.numero1 = Math.floor(Math.random() * 10);
+    this.numero2 = Math.floor(Math.random() * 10);
+    this.suma = this.numero1 + this.numero2;
+  }
 }
--- problema2/src/app/formulario/formulario.component.ts
+++ problema2/src/app/formulario/formulario.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
+  numero1: number = 0;
+numero2: number = 0;
+suma: number = 0;
+ 
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.generarNumerosAleatorios();
+  }
+
+  generarNumerosAleatorios(): void {
+    this.numero1 = Math.floor(Math.random() * 10);
+    this.numero2 = Math.floor(Math.random() * 10);
+    this.suma = this.numero1 + this.numero2;
+  }
 }
